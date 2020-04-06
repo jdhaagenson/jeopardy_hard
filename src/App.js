2d9bc34b1b01d0ac49d8e504a839da5cc5da7491
Jordan Haagenson <jdhaagenson@gmail.com>
2020-04-06 10:09:15 -0700

    lowercase jeopardy by mistake had to change whole word

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-// import Navigation from './Navigation'
 import Welcome from './components/Welcome';
 import Clock from "./components/clock/Clock";
 import Contact from "./components/contact/Contact";
 import Navigation from "./components/Navigation";
-
+import NotFound from './NotFound';
+import Jeopardy from './components/jeopardy/Jeo';
 
 function App ()  {
     return (
@@ -17,6 +17,8 @@ function App ()  {
             render={(props) => <Welcome {...props} name="Jordan"/>}/>
           <Route path="/clock" component={Clock}/>
           <Route path="/contact" component={Contact}/>
+          <Route path="/jeopardy" component={Jeopardy}/>
+          <Route path="*" component={NotFound}/>
         </Switch>
         <Navigation/>
       </div>
